bcddc24c6c8f14c3bb8f8a208d28c97204bc3a76
Yang Bo <atryyang@thoughtworks.com>
2016-04-14 21:54:10 +0800

    Add the initial implementation

---
+++ build.sbt
@@ -0,0 +1,21 @@
+organization := "com.thoughtworks.enableIf"
+
+name := "enableIf"
+
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
+
+libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+
+libraryDependencies += "com.lihaoyi" %% "utest" % "0.4.3" % Test
+
+testFrameworks += new TestFramework("utest.runner.Framework")
+
+libraryDependencies ++= {
+  if (scalaBinaryVersion.value == "2.10") {
+    Seq("org.scalamacros" %% "quasiquotes" % "2.1.0")
+  } else {
+    Seq()
+  }
+}
+
+crossScalaVersions := Seq("2.10.6", "2.11.8")
---
+++ src/main/scala/com/thoughtworks/enableIf.scala
@@ -0,0 +1,38 @@
+package com.thoughtworks
+
+import scala.annotation.StaticAnnotation
+
+import scala.reflect.macros.Context
+
+object enableIf {
+
+  def isEnabled(c: Context, booleanCondition: Boolean) = booleanCondition
+
+  def isEnabled(c: Context, functionCondition: Context => Boolean) = functionCondition(c)
+
+  private[enableIf] object Macros {
+    def macroTransform(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+      import c.universe._
+      val Apply(Select(Apply(_, List(condition)), _), List(_)) = c.macroApplication
+      if (c.eval(c.Expr[Boolean](
+        q"""
+          _root_.com.thoughtworks.enableIf.isEnabled(${reify(c).tree}, $condition)
+        """))) {
+        c.Expr(q"..${annottees.map(_.tree)}")
+      } else {
+        c.Expr(EmptyTree)
+      }
+    }
+  }
+
+}
+
+final class enableIf(condition: Context => Boolean) extends StaticAnnotation {
+
+  def this(condition: Boolean) = this { _ => condition }
+
+  import scala.language.experimental.macros
+
+  def macroTransform(annottees: Any*): Any = macro enableIf.Macros.macroTransform
+
+}
