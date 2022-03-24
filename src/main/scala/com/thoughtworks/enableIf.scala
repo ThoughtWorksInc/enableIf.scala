cc2639b7f590e6d701d0e7c0af45bab050da9e3a
Darcy Shen <da@tubi.tv>
2022-03-24 16:07:33 +0800

    Re-impl

--- src/main/scala/com/thoughtworks/enableIf.scala
+++ src/main/scala/com/thoughtworks/enableIf.scala
@@ -6,16 +6,33 @@ import scala.reflect.macros.Context
 import scala.util.matching.Regex
 
 object enableIf {
+  private def getArtifactIds(artifactId: String): (String, String) = {
+    val scalaMajorVersion = scala.util.Properties.versionNumberString.split("\\.")
+      .take(2).mkString(".")
+    val javaAID = artifactId.stripSuffix(scalaMajorVersion)
+    val scalaAID = s"${javaAID}_${scalaMajorVersion}"
+    (javaAID, scalaAID)
+  }
+
+  private def getRegexList(artifactId: String, regex: Regex): List[String] = {
+    val (javaAID, scalaAID) = getArtifactIds(artifactId)
+    List(s".*${javaAID}-${regex.toString}", s".*${scalaAID}-${regex.toString}")
+  }
+
+  private def getRegexList(artifactId: String, version: String): List[String] = {
+    val versionRegex = s"${version.replace(".", "\\.")}.*"
+    getRegexList(artifactId, new Regex(versionRegex))
+  }
+
   def hasArtifactInClasspath(artifactId: String, regex: Regex)(c: Context): Boolean = {
-    c.classPath.exists(
-      _.getPath.matches(s".*${artifactId}-${regex.toString}")
+    getRegexList(artifactId, regex).exists(regex =>
+      hasRegexInClasspath(regex)(c)
     )
   }
 
   def hasArtifactInClasspath(artifactId: String, version: String)(c: Context): Boolean = {
-    val versionRegex = version.replace(".", "\\.")
-    c.classPath.exists(
-      _.getPath.matches(s".*${artifactId}-${versionRegex}.*")
+    getRegexList(artifactId, version).exists(regex =>
+      hasRegexInClasspath(regex)(c)
     )
   }
 
@@ -36,18 +53,46 @@ object enableIf {
   def isEnabled(c: Context, functionCondition: Context => Boolean) =
     functionCondition(c)
 
+  private def evalHasRegexInClassPath(regexStr: String)(c: Context): Boolean = {
+    import c.universe._
+    val regex = Literal(Constant(regexStr))
+    c.eval(c.Expr[Boolean](q"""
+      _root_.com.thoughtworks.enableIf.hasRegexInClasspath(${regex})(${reify(c).tree})
+    """))
+  }
+
   private[enableIf] object Macros {
     def macroTransform(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
       import c.universe._
-      val Apply(Select(Apply(_, List(condition)), _), List(_ @_*)) =
-        c.macroApplication
-      if (
-        c.eval(c.Expr[Boolean](q"""
-          _root_.com.thoughtworks.enableIf.isEnabled(${reify(
-            c
-          ).tree}, $condition)
-        """))
-      ) {
+      val Apply(Select(Apply(_, List(origCondition)), _), List(_@_*)) = c.macroApplication
+      val condition = origCondition match {
+        case Apply(Ident(name), List(arg)) if "hasRegexInClasspath".equals(name.decoded) =>
+          val regex: String = arg match {
+            case Select(Tuple2(Literal(Constant(regexStr)), _)) => regexStr.asInstanceOf[String]
+            case Literal(Constant(regexStr)) => regexStr.asInstanceOf[String]
+            case _ =>
+              throw new IllegalArgumentException("hasRegexInClasspath only accepts String/Regex literals")
+          }
+          val result = evalHasRegexInClassPath(regex)(c)
+          Literal(Constant(result))
+        case Apply(Ident(name), List(Literal(Constant(artifactId)), arg)) if "hasArtifactInClasspath".equals(name.decoded) =>
+          val regexes = arg match {
+            case Select(Tuple2(Literal(Constant(regexStr)), _)) =>
+              getRegexList(artifactId.asInstanceOf[String], new Regex(regexStr.asInstanceOf[String]))
+            case Literal(Constant(version)) =>
+              getRegexList(artifactId.asInstanceOf[String], version.asInstanceOf[String])
+            case _ =>
+              throw new IllegalArgumentException("hasArtifactInClasspath only accepts String/Regex literals")
+          }
+          val result = regexes.exists(evalHasRegexInClassPath(_)(c))
+          Literal(Constant(result))
+        case _ =>
+          origCondition
+      }
+      if (c.eval(c.Expr[Boolean](
+        q"""
+          _root_.com.thoughtworks.enableIf.isEnabled(${reify(c).tree}, $condition)
+        """))) {
         c.Expr(q"..${annottees.map(_.tree)}")
       } else {
         c.Expr(EmptyTree)
--- src/test/scala/com/thoughtworks/EnableWithArtifactTest.scala
+++ src/test/scala/com/thoughtworks/EnableWithArtifactTest.scala
@@ -12,34 +12,29 @@ import org.scalatest.matchers.should.Matchers
  * @author 沈达 (Darcy Shen) &lt;sadhen@zoho.com&gt;
  */
 class EnableWithArtifactTest extends AnyFreeSpec with Matchers {
+  "Test if we are using quasiquotes explicitly" in {
 
-  "enableWithArtifact by artifactId and version" in {
+    object ExplicitQ {
 
-    object ShouldEnable {
-
-      @enableIf(c => hasArtifactInClasspath("scala", scala.util.Properties.versionNumberString)(c))
-      def whichIsEnabled = "good"
-
-      /**
-       * sbt is using the self-managed scala library, that's why we are using
-       * `scala` as artifactId and `2.12.15` as the version string
-       * eg. $HOME/.sbt/boot/scala-2.12.15/lib/scala-library.jar
-       *
-       * For most usages of enableWithArtifact, 3rd-party libraries should be used
-       */
-      @enableIf(c => hasArtifactInClasspath("scala-library", scala.util.Properties.versionNumberString)(c))
+      @enableIf(hasArtifactInClasspath("quasiquotes", "2.1.1"))
       def whichIsEnabled = "good"
     }
-    object ShouldDisable {
+    object ImplicitQ {
+      @enableIf(hasArtifactInClasspath("scala-library", "2\\.1[123]\\..*".r))
+      def whichIsEnabled = "bad"
 
-      @enableIf(c => hasArtifactInClasspath("scala-library", "0.0.0")(c))
+      @enableIf(hasArtifactInClasspath("scala", "2\\.1[123]\\..*".r))
       def whichIsEnabled = "bad"
     }
 
-    import ShouldEnable._
-    import ShouldDisable._
-    assert(whichIsEnabled == "good")
 
+    import ExplicitQ._
+    import ImplicitQ._
+    if (scala.util.Properties.versionNumberString < "2.11") {
+      assert(whichIsEnabled == "good")
+    } else {
+      assert(whichIsEnabled == "bad")
+    }
   }
 
   "Add TailRec.flatMap for Scala 2.10 " in {
